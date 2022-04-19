52d93c92f588480ca600d2eb9d54a0109b37ab69
Beaty <bmueni@student.mmust.ac.ke>
2022-04-19 06:42:26 -0700

    correction on comment

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,15 +2,19 @@
 /**
 * main - Entry point
 *
-* Return> Always 0 (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
+
 {
 int i;
+
 for (i = 0; i < 10; i++)
 {
 printf("%i", i);
 }
+
 printf("\n");
+
 return (0);
 }
