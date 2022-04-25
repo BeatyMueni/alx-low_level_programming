d7108586d660a8594b6608064a3a89c59bf8dbb1
Beaty <bmueni@student.mmust.ac.ke>
2022-04-25 06:58:56 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.c"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+void print_alphabet(void);
+for (print_alphabet = a; print_alphabet <= z; print_alphabet++)
+{
+_putchar('print_alphabet');
+_putchar('\n');
+}
+return (0);
+}
