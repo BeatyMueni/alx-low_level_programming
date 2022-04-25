b19847ac34a6bcfcf0bb2965010a2ee631b63826
Beaty <bmueni@student.mmust.ac.ke>
2022-04-25 07:14:09 -0700

    rectify

--- 0x01-variables_if_else_while/1-alphabet.c
+++ 0x01-variables_if_else_while/1-alphabet.c
@@ -1,16 +1,19 @@
-#include "main.c"
+#include "main.h"
 /**
  * main - Entry point
  *
  * Return: Always 0
  */
-int main(void)
-{
+
 void print_alphabet(void);
-for (print_alphabet = a; print_alphabet <= z; print_alphabet++)
 {
-_putchar('print_alphabet');
-_putchar('\n');
+char alpha;
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+_putchar(alpha);
 }
+_putchar('\n');
+
 return (0);
 }
