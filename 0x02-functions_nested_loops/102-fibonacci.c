b480b0677ae44262da91bb236a46e8a6a7751627
Beaty <bmueni@student.mmust.ac.ke>
2022-04-26 03:37:31 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+long int i, a = 1, b = 2, sum = 0;
+for ( 1 = 0; i < 49; i++)
+{
+printf("%ld, ", a);
+sum = a + b;
+a = b:
+b = sum;
+if (i == 48)
+printf("%ld\n", a);
+}
+return (0);
+}
