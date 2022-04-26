16738cc402242ff211cb30f8cd835a95a4441ba8
Beaty <bmueni@student.mmust.ac.ke>
2022-04-26 03:50:47 -0700

    even-valued terms

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+unsigned long int lim = 4000000;
+unsigned long int bef = 0;
+unsigned long int aft = 1;
+unsigned long int R = 0;
+unsigned long int sum = 0;
+
+}
