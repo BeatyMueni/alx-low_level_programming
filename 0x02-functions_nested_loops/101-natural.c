0bf12a7783c44cb14c27e8c88dfb00d1af4014e9
Beaty <bmueni@student.mmust.ac.ke>
2022-04-26 03:27:31 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int a = 1024, b, sum = 0;
+for (b = 0; b < a; b++)
+{
+if ((b % 3 == 0) || (b % 5 == 0))
+{
+sum = sum + b;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
