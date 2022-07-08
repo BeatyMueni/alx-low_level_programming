b9412808a9316423d1f4c0508ed67dff5dc14c0b
Beaty <bmueni@student.mmust.ac.ke>
2022-07-08 06:27:52 -0700

    check-betty documentation style

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for a digit 
+ * _isdigit - checks for a digit
  * @c: input
  * Return: 1 if c is digit 0 otherwise
  */
 
 int _isdigit(int c)
 {
-	if ((c >= 48) && ( c <= 57))
+	if ((c >= 48) && (c <= 57))
 		return (1);
 	else
 		return (0);
